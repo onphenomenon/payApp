2f5c5366070e3076e72c14f5f3fc7a945fa8f685
Karianne <karianne@mindflash.com>
2017-06-12 19:41:00 -0700

    Create card-edit.service.ts

--- src/app/card-edit.service.ts
+++ src/app/card-edit.service.ts
@@ -39,7 +39,6 @@ export class CardEditService {
   }
 
   private handleError(error: Response | any) {
-    // In a real world app, you might use a remote logging infrastructure
     let errMsg: string;
     if (error instanceof Response) {
       const body = error.json() || '';
